fb9d24ab56dd61d8a51bae36b26755108cb486d9
Ken Kellner <ken@kenkellner.com>
2024-08-03 07:57:00 -0400

    Refactor residuals method

---
+++ R/residuals.R
@@ -0,0 +1,36 @@
+# Method for all fit types, exported
+setMethod("residuals", "unmarkedFit", function(object, ...){
+  residuals_internal(object)
+})
+
+# Internal method for specific fit types, not exported
+setGeneric("residuals_internal", function(object){
+  standardGeneric("residuals_internal")
+})
+
+setMethod("residuals_internal", "unmarkedFit", function(object) {
+    y <- getY(object)
+    e <- fitted(object)
+    r <- y - e
+    return(r)
+})
+
+setMethod("residuals_internal", "unmarkedFitOccuMulti", function(object) {
+  res_list <- list()
+  ylist <- getY(object)
+  fitlist <- fitted(object)
+
+  for (i in seq_along(ylist)){
+    res_list[[i]] <- ylist[[i]] - fitlist[[i]]
+  }
+  names(res_list) <- names(ylist)
+  res_list
+})
+
+setMethod("residuals_internal", "unmarkedFitOccuTTD", function(object) {
+  tmax <- object@data@surveyLength
+  yraw <- object@data@y
+  y <- ifelse(yraw<tmax,1,0)
+  e <- fitted(object)
+  y - e
+})
