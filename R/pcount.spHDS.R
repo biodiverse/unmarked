b4da8c8d8c12ef3d32bda831baca05839ee7c8af
Ken Kellner <ken@kenkellner.com>
2025-03-27 09:52:09 -0400

    pcount.spHDS uses submodels

--- R/pcount.spHDS.R
+++ R/pcount.spHDS.R
@@ -7,122 +7,87 @@
 # unmarked model fitting function introduced in Section 9.8.4)
 # update 4/28/2017
 
-pcount.spHDS<-
-function (formula, data, K, mixture = c("P", "NB", "ZIP"), starts,
-    method = "BFGS", se = TRUE, ...)
-{
-    check_no_support(split_formula(formula))
+pcount.spHDS<- function (formula, data, K, mixture = c("P", "NB"), 
+  starts = NULL, method = "BFGS", se = TRUE, ...){
 
-    mixture <- match.arg(mixture, c("P", "NB", "ZIP"))
-    if (!is(data, "unmarkedFramePCount"))
-        stop("Data is not an unmarkedFramePCount object.")
-   engine<- "R"
-   #engine <- match.arg(engine, c("C", "R"))
-    if (identical(mixture, "ZIP") & identical(engine, "R"))
-        stop("ZIP mixture not available for engine='R'")
-    designMats <- getDesign(data, formula)
+  # Check arguments
+  forms <- split_formula(formula)
+  check_no_support(forms)
+  if (!is(data, "unmarkedFramePCount")){
+    stop("Data is not an unmarkedFramePCount object.")
+  }
+  mixture <- match.arg(mixture)
 
-    X <- designMats$X
-    V <- designMats$V
-    y <- designMats$y
-    X.offset <- designMats$X.offset
-    V.offset <- designMats$V.offset
+  # Build submodels
+  fam <- switch(mixture, P = "poisson", NB = "negative_binomial")
+  submodels <- unmarkedSubmodelList(
+    state = unmarkedSubmodelState(name = "Abundance", short_name = "lam", 
+                                  type = "state", formula = forms[[2]], data = data, 
+                                  family = fam, link = "log"),
 
-    if (is.null(X.offset))
-        X.offset <- rep(0, nrow(X))
-    if (is.null(V.offset))
-        V.offset <- rep(0, nrow(V))
-    NAmat <- is.na(y)
-    J <- ncol(y)
-    M <- nrow(y)
-    lamParms <- colnames(X)
-    detParms <- colnames(V)
-    nDP <- ncol(V)
-    nAP <- ncol(X)
-    if (missing(K)) {
-        K <- max(y, na.rm = TRUE) + 100
-        warning("K was not specified and was set to ", K, ".")
-    }
-    if (K <= max(y, na.rm = TRUE))
-        stop("specified K is too small. Try a value larger than any observation")
-    k <- 0:K
-    lk <- K + 1
-    M <- nrow(y)
-    J <- ncol(y)
-    k.ik <- rep(k, M)
-    k.ijk <- rep(k, M * J)
-    nP <- nAP + nDP + (mixture != "P")
-    if (!missing(starts) && length(starts) != nP)
-        stop(paste("The number of starting values should be",
-            nP))
-    if (identical(engine, "R")) {
+    det = unmarkedSubmodelDet(name = "Detection", short_name = "p", 
+                              type = "det", formula = forms[[1]], data = data, 
+                              family = "binomial", link = "logit")
+  )
 
-        y.ij <- as.numeric(t(y))
-        y.ijk <- rep(y.ij, each = K + 1)
-        navec <- is.na(y.ijk)
-        ijk <- expand.grid(k = 0:K, j = 1:J, i = 1:M)
-        ijk.to.ikj <- with(ijk, order(i, k, j))
-        nll <- function(parms) {
-            # Force the scale parameter to be positive. This is 1/(2*sigma*sigma)
-            tmp.parms<- parms[(nAP + 1):(nAP + nDP)]
-            tmp.parms[1]<- -1*exp(tmp.parms[1])
-            parms[(nAP + 1):(nAP + nDP)]<- tmp.parms
+  if(mixture == "NB"){
+    submodels['alpha'] <- unmarkedSubmodelScalar(name = "Dispersion", 
+                                                 short_name = "alpha", 
+                                                 type = "alpha", link = "log")
+  }
 
-            theta.i <- exp(X %*% parms[1:nAP] + X.offset)
-            p.ij <- 2*plogis(V %*% (parms[(nAP + 1):(nAP + nDP)]) +
-                V.offset)
-            theta.ik <- rep(theta.i, each = K + 1)
-            p.ijk <- rep(p.ij, each = K + 1)
-            bin.ijk <- dbinom(y.ijk, k.ijk, p.ijk)
-  #          if(any(is.nan(bin.ijk))) browser()
-            bin.ijk[which(is.na(bin.ijk))] <- 1
-            bin.ik.mat <- matrix(bin.ijk[ijk.to.ikj], M * (K +
-                1), J, byrow = TRUE)
-            g.ik <- rowProds(bin.ik.mat)
-            if (identical(mixture, "P")) {
-                f.ik <- dpois(k.ik, theta.ik)
-            }
-            else if (identical(mixture, "NB")) {
-                f.ik <- dnbinom(k.ik, mu = theta.ik, size = exp(parms[nP]))
-            }
-            dens.i.mat <- matrix(f.ik * g.ik, M, K + 1, byrow = TRUE)
-            dens.i <- rowSums(dens.i.mat)
-            -sum(log(dens.i))
-        }
-    }
+  # Build response object
+  response <- unmarkedResponseCount(data, submodels, Kmax = K)
 
-    if (missing(starts))
-        starts <- rep(0, nP)
-    fm <- optim(starts, nll, method = method, hessian = se, ...)
-    ests <- fm$par
-    nbParm <- switch(mixture, NB = "alpha", ZIP = "psi", P = character(0))
-    names(ests) <- c(lamParms, detParms, nbParm)
-    covMat <- invertHessian(fm, nP, se)
-    fmAIC <- 2 * fm$value + 2 * nP
-    stateName <- "Abundance"
-    stateEstimates <- unmarkedEstimate(name = stateName, short.name = "lam",
-        estimates = ests[1:nAP], covMat = as.matrix(covMat[1:nAP,
-            1:nAP]), invlink = "exp", invlinkGrad = "exp")
-    detEstimates <- unmarkedEstimate(name = "Detection", short.name = "p",
-        estimates = ests[(nAP + 1):(nAP + nDP)], covMat = as.matrix(covMat[(nAP +
-            1):(nAP + nDP), (nAP + 1):(nAP + nDP)]), invlink = "logistic",
-        invlinkGrad = "logistic.grad")
-    estimateList <- unmarkedEstimateList(list(state = stateEstimates,
-        det = detEstimates))
-    if (identical(mixture, "NB")) {
-        estimateList@estimates$alpha <- unmarkedEstimate(name = "Dispersion",
-            short.name = "alpha", estimates = ests[nP], covMat = as.matrix(covMat[nP,
-                nP]), invlink = "exp", invlinkGrad = "exp")
-    }
-    if (identical(mixture, "ZIP")) {
-        estimateList@estimates$psi <- unmarkedEstimate(name = "Zero-inflation",
-            short.name = "psi", estimates = ests[nP], covMat = as.matrix(covMat[nP,
-                nP]), invlink = "logistic", invlinkGrad = "logistic.grad")
-    }
-    umfit <- new("unmarkedFitPCount", fitType = "pcount", call = match.call(),
-        formula = formula, data = data, sitesRemoved = designMats$removed.sites,
-        estimates = estimateList, AIC = fmAIC, opt = fm, negLogLike = fm$value,
-        nllFun = nll, K = K, mixture = mixture)
-    return(umfit)
+  inputs <- nll_inputs(response, submodels, engine = "R")
+
+  # Fit model
+  fit <- fit_model(nll_pcount.spHDS_R, inputs = inputs, submodels = submodels,
+                   starts = starts, method = method, se = se, ...)
+
+  new("unmarkedFitPCount", fitType = "pcount", call = match.call(),
+      formula = formula, data = data, sitesRemoved = removed_sites(response),
+      estimates = fit$submodels, AIC = fit$AIC, opt = fit$opt, 
+      negLogLike = fit$opt$value, nllFun = fit$nll, 
+      K = response@Kmax, mixture = mixture)
 }
 
+
+nll_pcount.spHDS_R <- function(params, inputs){
+  with(inputs, {
+
+  beta_state <- params[idx_state[1]:idx_state[2]]
+  beta_det <- params[idx_det[1]:idx_det[2]]
+  beta_det[1] <- -1 * exp(beta_det[1])
+
+  M <- nrow(y)
+  J <- ncol(y)
+
+  k <- 0:Kmax
+  k.ik <- rep(k, M)
+  k.ijk <- rep(k, M * J)
+  y.ij <- as.numeric(t(y))
+  y.ijk <- rep(y.ij, each = Kmax + 1)
+  ijk <- expand.grid(k = 0:Kmax, j = 1:J, i = 1:M)
+  ijk.to.ikj <- with(ijk, order(i, k, j))
+
+  theta.i <- exp(X_state %*% beta_state + offset_state)
+  p.ij <- 2 * plogis(X_det %*% beta_det + offset_det)
+  
+  theta.ik <- rep(theta.i, each = Kmax + 1)
+  p.ijk <- rep(p.ij, each = Kmax + 1)
+  bin.ijk <- dbinom(y.ijk, k.ijk, p.ijk)
+
+  bin.ik.mat <- matrix(bin.ijk[ijk.to.ikj], M * (Kmax + 1), J, byrow = TRUE)
+  g.ik <- rowProds(bin.ik.mat)
+  if (family_state == 1) { # Poisson
+    f.ik <- dpois(k.ik, theta.ik)
+  } else if (family_state == 2) { # negbin
+    alpha <- exp(params[idx_alpha[1]])
+    f.ik <- dnbinom(k.ik, mu = theta.ik, size = alpha)
+  }
+  dens.i.mat <- matrix(f.ik * g.ik, M, Kmax + 1, byrow = TRUE)
+  dens.i <- rowSums(dens.i.mat)
+  -sum(log(dens.i), na.rm=TRUE)
+  })
+}
