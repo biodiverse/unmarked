5cb289b5ed6c83dc79d5d6a0d65aca39060ea258
Ken Kellner <ken@kenkellner.com>
2025-04-30 13:52:42 -0400

    Initial working colext refactor

---
+++ R/colext2.R
@@ -0,0 +1,182 @@
+colext2 <- function(psiformula = ~ 1, gammaformula = ~ 1,
+                    epsilonformula = ~ 1, pformula = ~ 1,
+                    data, starts, method = "BFGS", se = TRUE, ...){
+
+  ## truncate to 1
+  data@y <- truncateToBinary(data@y)
+  M <- numSites(data)
+  T <- data@numPrimary
+  J <- ncol(data@y) / T
+    
+  formula <- list(psiformula = psiformula, gammaformula = gammaformula,
+                  epsilonformula = epsilonformula, pformula = pformula)
+  check_no_support(formula)
+  designMats <- getDesign(data, formula = as.formula(paste(unlist(formula), collapse=" ")))
+  X_psi <- designMats$W
+  X_gam <- designMats$X.gam
+  X_eps <- designMats$X.eps
+  X_det <- designMats$V
+  y <- designMats$y
+  M <- nrow(y)
+  T <- ncol(y) / J
+  psiParms <- colnames(X_psi)
+  gamParms <- colnames(X_gam)
+  epsParms <- colnames(X_eps)
+  detParms <- colnames(X_det)
+
+  ## remove final year from transition prob design matrices
+  X_gam <- as.matrix(X_gam[-seq(T,M*T,by=T),])
+  X_eps <- as.matrix(X_eps[-seq(T,M*T,by=T),])
+
+  # Determine which periods were sampled at each site
+  pers_sampled <- matrix(1, M, T)
+  Trep <- rep(1:T, each = J)
+  for (t in 1:T){
+    ind <- which(Trep == t)
+    for (i in 1:M){
+      if(all(is.na(y[i, ind]))) pers_sampled[i,t] <- 0
+    }
+  }
+  T_site <- apply(pers_sampled, 1, sum)
+
+  Tsamp <- matrix(NA, M, T)
+  for (i in 1:M){
+    which_sampled <- which(pers_sampled[i,] == 1)
+    Tsamp[i,1:length(which_sampled)] <- which_sampled
+  }
+
+  no_detects <- matrix(1, M, T)
+  for (i in 1:M){
+    for (t in 1:T){
+      ind <- which(Trep == t)
+      ysub <- y[i, ind]
+      if(all(is.na(ysub))) next
+      ysub <- na.omit(ysub)
+      if(all(ysub == 0)){
+        no_detects[i,t] <- 1
+      } else {
+        no_detects[i,t] <- 0
+      }
+    }
+  }
+
+  nDP <- length(detParms)
+  nGP <- length(gamParms)
+  nEP <- length(epsParms)
+  nSP <- length(psiParms)
+  pind_mat <- matrix(0, 4, 2)
+  pind_mat[1,] <- c(1, length(psiParms))
+  pind_mat[2,] <- max(pind_mat[1,]) + c(1, length(gamParms))
+  pind_mat[3,] <- max(pind_mat[2,]) + c(1, length(epsParms))
+  pind_mat[4,] <- max(pind_mat[3,]) + c(1, length(detParms))
+  #nDMP <-  1
+  
+  tmb_dat <- list(y = as.vector(t(y)), X_psi = X_psi, X_gam = X_gam, X_eps = X_eps, X_det = X_det,
+                  M = M, T = T, J = J, site_sampled = pers_sampled, nd = no_detects)
+
+  tmb_pars <- list(beta_psi = rep(0, nSP), beta_gam = rep(0, nGP),
+                   beta_eps = rep(0, nEP), beta_det = rep(0, nDP))
+
+
+
+  #library(TMB)  
+  #compile("~/downloads/colext.cpp")
+  #dyn.load(dynlib("~/downloads/colext"))
+
+  #tmb_obj <- MakeADFun(data = tmb_dat, parameters=tmb_pars,
+  #                     DLL = "colext", silent=TRUE)
+
+  tmb_obj <- TMB::MakeADFun(data = c(model = "tmb_colext", tmb_dat), 
+                            parameters = tmb_pars,
+                            DLL = "unmarked_TMBExports", silent=TRUE)
+  
+  opt <- optim(unlist(tmb_pars), fn=tmb_obj$fn, gr=tmb_obj$gr, 
+               method=method, hessian = se)#, ...)
+
+  fmAIC <- 2 * opt$value + 2 * length(unlist(tmb_pars))
+
+  sdr <- TMB::sdreport(tmb_obj)
+
+  psi_coef <- get_coef_info(sdr, "psi", psiParms, pind_mat[1,1]:pind_mat[1,2])
+  gam_coef <- get_coef_info(sdr, "gam", gamParms, pind_mat[2,1]:pind_mat[2,2])
+  eps_coef <- get_coef_info(sdr, "eps", epsParms, pind_mat[3,1]:pind_mat[3,2])
+  det_coef <- get_coef_info(sdr, "det", detParms, pind_mat[4,1]:pind_mat[4,2])
+  
+  psi <- unmarkedEstimate(name = "Initial", short.name = "psi",
+                          estimates = psi_coef$ests,
+                          covMat = psi_coef$cov,
+                          invlink = "logistic",
+                          invlinkGrad = "logistic.grad")
+
+  col <- unmarkedEstimate(name = "Colonization", short.name = "col",
+                          estimates = gam_coef$ests,
+                          covMat = gam_coef$cov,
+                          invlink = "logistic",
+                          invlinkGrad = "logistic.grad")
+
+  ext <- unmarkedEstimate(name = "Extinction", short.name = "ext",
+                          estimates = eps_coef$ests,
+                          covMat = eps_coef$cov,
+                          invlink = "logistic",
+                          invlinkGrad = "logistic.grad")
+
+  det <- unmarkedEstimate(name = "Detection", short.name = "p",
+                          estimates = det_coef$ests,
+                          covMat = det_coef$cov,
+                          invlink = "logistic",
+                          invlinkGrad = "logistic.grad")
+
+  estimateList <- unmarkedEstimateList(list(psi = psi, col = col,
+                                            ext = ext, det=det))
+  
+  psis <- plogis(X_psi %*% psi_coef$ests)
+
+  ## computed projected estimates
+  phis <- array(NA,c(2,2,T-1,M))
+  phis[,1,,] <- plogis(X_gam %*% gam_coef$ests)
+  phis[,2,,] <- plogis(X_eps %*% -eps_coef$ests)
+
+  projected <- array(NA, c(2, T, M))
+  projected[1,1,] <- 1 - psis
+  projected[2,1,] <- psis
+  for(i in 1:M) {
+    for(t in 2:T) {
+      projected[,t,i] <- phis[,,t-1,i] %*% projected[,t-1,i]
+    }
+  }
+  projected.mean <- apply(projected, 1:2, mean)
+  rownames(projected.mean) <- c("unoccupied","occupied")
+  colnames(projected.mean) <- 1:T
+
+  ## smoothing (not supported right now)
+  smoothed <- array(NA, c(2, T, M))
+  smoothed.mean <- projected.mean
+  smoothed.mean[] <- NA
+  #forward(detParams, phis, psis, storeAlpha = TRUE)
+  #  beta <- backward(detParams, phis)
+  #  gamma <- array(NA, c(K + 1, nY, M))
+  #  for(i in 1:M) {
+  #  for(t in 1:nY) {
+  #      gamma[,t,i] <- alpha[,t,i]*beta[,t,i] / sum(alpha[,t,i]*beta[,t,i])
+  #  }}
+  #  smoothed.mean <- apply(gamma, 1:2, mean)
+  #  rownames(smoothed.mean) <- c("unoccupied","occupied")
+  #  colnames(smoothed.mean) <- 1:nY
+
+  umfit <- new("unmarkedFitColExt", fitType = "colext",
+                call = match.call(),
+                formula = as.formula(paste(unlist(formula),collapse=" ")),
+                psiformula = psiformula,
+                gamformula = gammaformula,
+                epsformula = epsilonformula,
+                detformula = pformula,
+                data = data, sitesRemoved = designMats$removed.sites,
+                estimates = estimateList,
+                AIC = fmAIC, opt = opt, negLogLike = opt$value,
+                nllFun = tmb_obj$fn,
+                projected = projected,
+                projected.mean = projected.mean,
+                smoothed = smoothed, smoothed.mean = smoothed.mean)
+
+  return(umfit)
+}
---
+++ src/TMB/tmb_colext.hpp
@@ -0,0 +1,128 @@
+#undef TMB_OBJECTIVE_PTR
+#define TMB_OBJECTIVE_PTR obj
+
+// name of function below **MUST** match filename
+template <class Type>
+Type tmb_colext(objective_function<Type>* obj) {
+
+  DATA_VECTOR(y);
+  DATA_MATRIX(X_psi);
+  DATA_MATRIX(X_gam);
+  DATA_MATRIX(X_eps);
+  DATA_MATRIX(X_det);
+  DATA_INTEGER(M);
+  DATA_INTEGER(T);
+  DATA_INTEGER(J);
+  DATA_MATRIX(site_sampled);
+  DATA_MATRIX(nd);
+
+  PARAMETER_VECTOR(beta_psi);
+  PARAMETER_VECTOR(beta_gam);
+  PARAMETER_VECTOR(beta_eps);
+  PARAMETER_VECTOR(beta_det);
+
+  Type loglik = 0.0;
+
+  vector<Type> psi = X_psi * beta_psi;
+  psi = invlogit(psi);
+
+  vector<Type> gam = X_gam * beta_gam;
+  gam = invlogit(gam);
+
+  vector<Type> eps = X_eps * beta_eps;
+  eps = invlogit(eps);
+
+  vector<Type> p = X_det * beta_det;
+  p = invlogit(p);
+
+  int Tidx = 0;
+  int pind = 0;
+
+  vector<Type> Dy(2);
+  matrix<Type> phi_total(2, 2);
+  matrix<Type> phi(2, 2);
+  vector<Type> phi_end(2);
+  vector<Type> psi_vec(2);
+  matrix<Type> Dy_diag(2,2);
+  Dy_diag = Dy_diag.setZero();
+
+  for (int i=0; i<M; i++){
+
+    phi_total = phi.setIdentity();
+
+    //All but the last time period
+    for (int t=0; t<(T-1); t++){
+
+      phi = phi.setZero();
+      //phi(0, 0) = 1 - eps(Tidx);
+      //phi(1, 0) = eps(Tidx);
+      //phi(0, 1) = gam(Tidx);
+      //phi(1, 1) = 1 - gam(Tidx);
+      phi(0, 0) = 1 - eps(Tidx);
+      phi(0, 1) = eps(Tidx);
+      phi(1, 0) = gam(Tidx);
+      phi(1, 1) = 1 - gam(Tidx);
+      Tidx += 1;
+
+      if(site_sampled(i, t) == 1){
+        Dy = Dy.setOnes();
+
+        if(nd(i, t) == 0){ // if at least one detection
+          Dy(1) = 0;
+        }
+
+        for (int j=0; j<J; j++){
+          if(R_IsNA(asDouble(y(pind)))){
+            pind += 1;
+            continue;
+          }
+
+          Dy(0) *= pow(p(pind), y(pind)) * pow(1-p(pind), 1-y(pind));
+          pind += 1;
+        }
+        Dy_diag.diagonal() = Dy;
+        //phi_total = phi_total * phi * Dy_diag;
+        phi_total = phi_total * Dy_diag * phi;
+      } else {
+        phi_total = phi_total * phi;
+        pind += J;
+      }
+    }
+    
+    //Final period; no psi calculation
+    if(site_sampled(i, (T-1)) == 1){
+      Dy = Dy.setOnes();
+
+      if(nd(i, (T-1)) == 0){ // if at least one detection
+        Dy(1) = 0;
+      }
+
+      for (int j=0; j<J; j++){
+        if(R_IsNA(asDouble(y(pind)))){
+          pind += 1;
+          continue;
+        }
+
+        Dy(0) *= pow(p(pind), y(pind)) * pow(1-p(pind), 1-y(pind));
+        pind += 1;
+      }
+
+      phi_end = phi_total * Dy;
+    } else {
+      Dy = Dy.setOnes();
+      phi_end = phi_total * Dy;
+      pind += J;
+    }
+
+    psi_vec(0) = psi(i);
+    psi_vec(1) = 1 - psi(i);
+    //Surely TMB has a better dot product??
+    vector<Type> site_lik = psi_vec.array() * phi_end.array();
+    loglik += log(sum(site_lik));
+  }
+
+  return(-loglik);
+}
+
+#undef TMB_OBJECTIVE_PTR
+#define TMB_OBJECTIVE_PTR this
